e4376bad15989609d58f982188a2a6b5fa6ad315
karan parmar <135197001+karanparmar141@users.noreply.github.com>
2023-08-31 12:26:03 +0530

    create men pages

--- Script/men.js
+++ Script/men.js
@@ -1,2 +1,11 @@
 import nav from "../Components/nav.js";
 document.getElementById("navbar").innerHTML=nav();
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 500) {
+      $("header").addClass("fixed")
+    }
+    else {
+      $("header").removeClass("fixed")
+    }
+  })
