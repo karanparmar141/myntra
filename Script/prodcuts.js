61e1c26392d959adfee147328620041c5e60d5dc
karan parmar <135197001+karanparmar141@users.noreply.github.com>
2023-09-03 18:32:10 +0530

    men.html

--- Script/prodcuts.js
+++ Script/prodcuts.js
@@ -152,75 +152,6 @@ const ui = (data) => {
   })
 }
 
-const allui = (data) => {
-  document.getElementById("pro").innerHTML=""
-  data.map((prodcut) => {
-    p += 1
-    console.log(prodcut.rating)
-
-    let cardimg = document.createElement("div")
-    cardimg.innerHTML = `
-        <div id="slider${p}" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                          <div class="carousel-item active">
-                            <img src="${prodcut.img1}" class="card-img-top poition-relative" alt="1...">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="${prodcut.img2}" class="card-img-top poition-relative" alt="2...">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="${prodcut.img3}" class="card-img-top poition-relative" alt="3...">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="${prodcut.img4}" class="card-img-top poition-relative" alt="3...">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="${prodcut.img5}" class="card-img-top poition-relative" alt="3...">
-                          </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#slider${p}" data-bs-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#slider${p}" data-bs-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-        `
-    let cardtitle = document.createElement("h5")
-    cardtitle.classList.add("fw-bold", "text-uppercase", "m-0")
-    cardtitle.innerHTML = prodcut.prodcuts_name
-
-    let cardcat = document.createElement("p")
-    cardcat.classList.add("fw-bold", "text-uppercase", "m-0")
-    cardcat.innerHTML = prodcut.prodcuts_deatil
-    let rate=document.createElement("p")
-    rate.classList.add("fw-semibold")
-    rate.innerHTML=`${prodcut.rating.rate} <i class="bi bi-star-fill"></i>  |  ${prodcut.rating.count}`
-    if(prodcut.rating.rate >4){
-        rate.classList.add("text-primary")
-    }
-    else if(prodcut.rating.rate>3){
-        rate.classList.add("text-success")
-    }
-    else if(prodcut.rating.rate>2){
-        rate.classList.add("text-warning")
-    } 
-    else{
-        rate.classList.add("text-danger")
-    }
-    let card = document.createElement("div")
-    card.classList.add("card", "border-0", "shadow", "text-center")
-    card.append(cardimg, cardtitle,rate, cardcat)
-    let div = document.createElement("div")
-    div.classList.add("col-md-3", "col-6", "my-2")
-    div.append(card)
-
-
-    document.getElementById("pro").append(div)
-  })
-}
 
 const getpro = (prodcuts) => {
   console.log(prodcuts)
@@ -229,16 +160,26 @@ const getpro = (prodcuts) => {
     .then((data) => { ui(data) })
 }
 
+
+const brapro = (brand) => {
+  console.log(brand)
+  // fetch(`http://localhost:3000/prodcuts?brand=${brand}`)
+  //   .then((res) => res.json())
+  //   .then((data) => { ui(data) })
+}
+
+
+
 const allpro = () => {
   fetch(`http://localhost:3000/prodcuts`)
     .then((res) => res.json())
-    .then((data) => { allui(data) })
+    .then((data) => { ui(data) })
 }
 
 // brand 
 
 
-let brand = ["gant", "android"];
+let brand = [ "android","gant","test"];
 
 for (let i = 0; i < brand.length; i++) {
   let input = document.createElement("input");
@@ -255,9 +196,7 @@ for (let i = 0; i < brand.length; i++) {
 }
 
 for (let i = 0; i < brand.length; i++) {
-  document
-    .getElementById(brand[i])
-    .addEventListener("click", () => getpro(brand[i]));
+  document.getElementById(brand[i]).addEventListener("click", ()=>brapro(brand[i]));
 }
 
 // categories
@@ -300,6 +239,7 @@ document.getElementById("price").addEventListener("submit",(e)=>{
       const price = data.filter(product => {
         return product.orginal_price >= minprice && product.orginal_price <= maxprice;
       });
+      console.log(price)
       ui(price)
     })
 })
